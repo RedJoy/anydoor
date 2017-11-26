7ad899296579d84d57137e6a615524e543041122
redjoy <redjoy@Red-Joy.local>
2017-11-26 23:23:26 +0800

    start the service use promisify module

--- src/app.js
+++ src/app.js
@@ -1,38 +1,12 @@
 const http = require('http');
 const chalk = require('chalk');
 const path = require('path');
-const fs = require('fs');
 const conf = require('./config/defaultConfig.js');
-
+const route = require('./helper/route');
 const server = http.createServer( (req,res) => {
 	const url = req.url;
 	const filePath = path.join(conf.root, req.url);
-	fs.stat(filePath, (err,stats) => {
-		if(err) {
-			res.statusCode = 404;
-			res.setHeader('Content-Type','text/plain');
-			res.end(`${filePath} is not a directory or file`);
-			return;
-		}
-		// 如果是文件
-		if(stats.isFile()) {
-			res.statusCode = 200;
-			res.setHeader('Content-Type','text/plain');
-			// fs.readFile(filePath, (err,data) => {
-			// 	res.end(data);
-			// });  //不介意使用读的速度过慢
-			fs.createReadStream(filePath).pipe(res); //用流的方式读写
-		}else if (stats.isDirectory()) {
-			fs.readdir(filePath, (err,files) => {
-				res.statusCode = 200;
-				res.setHeader('Content-Type','text/plain');
-				res.end(files.join(','));
-			});
-		}
-	});
-	// res.statusCode = 200;
-	// res.setHeader('Content-Type','text/html');
-	// res.end(filePath);
+	route(req,res,filePath);
 });
 
 server.listen( conf.port , conf.hostname , () => {
